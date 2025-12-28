ec20cf17b716533f41bfe1a12ba6967311c4aefd
SUN-Xiaoying <xiaoyingsun523@gmail.com>
2025-12-28 20:21:12 +0100

    20251228 Two City Scheduling

--- greedy/schedule/two_city_sched_cost.py
+++ greedy/schedule/two_city_sched_cost.py
@@ -1,5 +1,16 @@
 # https://leetcode.com/problems/two-city-scheduling/description/
+from typing import List
 
 
 class Solution:
+    # 0 ms Beats 100.00%
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        sum = 0
+        arr = []
+        for cost in costs:
+            arr.append(cost[1]- cost[0])
+            sum += cost[0]
+        arr.sort()
+        for i in range(len(costs)/2):
+            sum += arr[i]
+        return sum
